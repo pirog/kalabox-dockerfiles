6067e4515c5d2ca848e5b7c66c37b4f6e05b5ad2
Mike Pirog <mike@kalamuna.com>
2015-07-01 19:28:34 -0700

    #232: Update maria and nginx

--- nginx/start.sh
+++ nginx/start.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 
-PHP_HOST=$(dig +short php.${APPDOMAIN})
-sed -i "s/fastcgi_pass .*/fastcgi_pass ${PHP_HOST}:9000;/g" /src/config/nginx/site.conf
-
-nginx
+nginx -v
+nginx -g "daemon off;"
